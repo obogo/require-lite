af4f0390961bdf5765b99589bc3f01377f0ebf3c
Robert Taylor <roboncode@gmail.com>
2016-06-16 10:02:52 -0600

    initial commit

---
+++ dist/require-lite.js
@@ -0,0 +1,123 @@
+var define, require;
+(function () {
+    var _get, defined, pending, definitions, timer = 0;
+    var CACHE_TOKEN = '~', DEFINITIONS_TOKEN = '.', REQUIRE = 'require', EXPORTS = 'exports', DEFAULT = 'default', CACHED = 'c', DEFINED = 'd', PENDING = 'p';
+    /**
+     * Sets and Gets cache, defined, and pending items in a private internal cache
+     */
+    _get = Function[CACHE_TOKEN] = Function[CACHE_TOKEN] || function (name) {
+        if (!_get[name]) {
+            _get[name] = {};
+        }
+        return _get[name];
+    };
+    definitions = _get(CACHED); // these are items that have been initialized and permanently cached
+    defined = _get(DEFINED); // these are items that have been defined but have not been initialized
+    pending = _get(PENDING); // these are items that have been initialized but have deps that need initialized before done
+    /**
+     * Initializes
+     */
+    function initDefinition(name) {
+        if (!defined[name]) {
+            var args = arguments;
+            var val = args[1];
+            if (typeof val === 'function') {
+                // ex. define('myFunc', function(){...});
+                defined[name] = val(); // invoke immediately and assign to defined
+            }
+            else {
+                // store in a temporary definitions until all definitions have been processed
+                // ex. define('myFunc', ['toBoolean'], function(toBoolean){...})
+                definitions[name] = args[2]; // skip array and assign funtion to cached name
+                definitions[name][DEFINITIONS_TOKEN] = val; // assign dependencies to definitions on function itself
+            }
+        }
+    }
+    ;
+    function resolveModule(name, initHandler) {
+        pending[name] = true; // mark this definition as pending
+        var deps = initHandler[DEFINITIONS_TOKEN]; // get any dependencies required by definition
+        var args = [];
+        var i, len;
+        var dependencyName;
+        if (deps) {
+            len = deps.length;
+            for (i = 0; i < len; i++) {
+                dependencyName = deps[i];
+                if (definitions[dependencyName]) {
+                    if (!pending.hasOwnProperty(dependencyName)) {
+                        resolveModule(dependencyName, definitions[dependencyName]);
+                    }
+                    resolveModule(dependencyName, definitions[dependencyName]);
+                    delete definitions[dependencyName];
+                }
+            }
+        }
+        if (!defined.hasOwnProperty(name)) {
+            var exports;
+            var module;
+            for (i = 0; i < len; i++) {
+                dependencyName = deps[i]; // get the dependency name
+                if (dependencyName === REQUIRE) {
+                    args.push(require);
+                }
+                else if (dependencyName === EXPORTS) {
+                    exports = {};
+                    args.push(exports);
+                }
+                else if (defined.hasOwnProperty(dependencyName)) {
+                    args.push(defined[dependencyName]); // this will push an item even if it is undefined
+                }
+                else if (!require.ignoreWarnings) {
+                    args.push(undefined);
+                    console.warn('Module "' + name + '" requires "' + dependencyName + '", but is undefined.');
+                }
+            }
+            var returnVal = initHandler.apply(null, args); // call the function and assign return value onto defined list
+            if (exports) {
+                if (exports.hasOwnProperty(DEFAULT)) {
+                    defined[name] = exports[DEFAULT];
+                }
+                else {
+                    defined[name] = exports;
+                }
+            }
+            else {
+                defined[name] = returnVal;
+            }
+        }
+        delete pending[name]; // permanently remove pending item
+    }
+    function resolve() {
+        for (var name in definitions) {
+            if (definitions.hasOwnProperty(name)) {
+                resolveModule(name, definitions[name]);
+            }
+        }
+    }
+    define = function (name, deps, initHandler) {
+        if (deps === void 0) { deps = []; }
+        initDefinition.apply(null, arguments);
+        clearInterval(timer);
+        setTimeout(resolve);
+    };
+    require = function (modules, handler) {
+        clearTimeout(timer);
+        resolve();
+        if (!handler) {
+            var name = modules.toString();
+            return defined[name];
+        }
+        var args = [];
+        if (typeof modules === 'string') {
+            modules = [modules];
+        }
+        var len = modules.length;
+        for (var i = 0; i < len; i++) {
+            args.push(defined[modules[i]]);
+        }
+        handler.apply(null, args);
+    };
+    require.ignoreWarnings = false;
+    return define;
+}());
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var ts = require('gulp-typescript');
+
+// options
+var tsFiles = 'require-lite.ts';
+var sourceFiles = 'require-lite.js';
+var destinationFolder = 'dist';
+
+// tasks
+gulp.task('compile', function() {
+    return gulp.src(tsFiles, {cwd: "src"})
+        .pipe(ts({
+            noImplicitAny: false,
+            module: "amd"
+        }))
+        .pipe(gulp.dest('src'));
+});
+
+gulp.task('minify', function () {
+    return gulp.src(sourceFiles, {cwd: "src"})
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(concat(sourceFiles))
+        .pipe(gulp.dest(destinationFolder)) // save .js
+        .pipe(uglify({ preserveComments: 'license' }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest(destinationFolder)); // save .min.js
+});
+
+gulp.task('default', ['compile', 'minify']);
