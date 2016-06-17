76b87c5615a5663f378801e2988856c8fe781cbb
Robert Taylor <roboncode@gmail.com>
2016-06-17 11:54:43 -0600

    init unit test specs

---
+++ tests/spec/default_spec.js
@@ -0,0 +1,12 @@
+describe('require-lite', function () {
+
+    it('define() should be a type function', function () {
+        expect(typeof define).toBe('function');
+    });
+
+    it('require() should be a type function', function () {
+        expect(typeof define).toBe('function');
+    });
+
+
+});
